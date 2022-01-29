5cddb98eea40d45a8b9699910b2c35a6198ad166
kadersahib <kadersahib@localhost>
2022-01-29 13:10:27 +0530

    if class

---
+++ kadercmk/kader/ifclass.java
@@ -0,0 +1,19 @@
+package kader;
+import java.util.Scanner;
+
+public class ifclass {
+
+	public static void main(String[] args) {
+		int num =4;
+		if (num==10) {
+			System.out.println("is equal");
+		}else if (num>10) {
+			System.out.println("greater than 10");
+		}
+		else {
+			System.out.println("lesser than 10");
+		}
+		
+	}
+
+}
